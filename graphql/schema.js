2fee083004932d16dd9cee53fce9df41d0024d54
Jacob Goskha <jacobgoskha@gmail.com>
2019-10-23 17:19:26 -0400

    queries: pastLaunches and Rockets added

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,6 +1,21 @@
 const axios = require('axios');
 /* const PastLaunchesType = require('./pastLaunchType').PastLaunchesType; */
 
+const data = [
+  { id: '1', name: 'Jacob', address: { city: 'toronto', province: 'Ontario' } },
+  { id: '1', name: 'Sam', address: { city: 'Chicago', province: 'Illinois' } },
+  {
+    id: '1',
+    name: 'George',
+    address: { city: 'Halifax', province: 'Nova Scotia' }
+  },
+  {
+    id: '1',
+    name: 'Bob',
+    address: { city: 'Boston ', province: 'Massechussets' }
+  }
+];
+
 // The construction pillers of GraphQL
 const {
   GraphQLSchema,
@@ -8,7 +23,8 @@ const {
   GraphQLList,
   GraphQLString,
   GraphQLInt,
-  GraphQLNonNull
+  GraphQLNonNull,
+  GraphQLBoolean
 } = require('graphql');
 
 const PastLaunchesType = new GraphQLObjectType({
@@ -16,8 +32,19 @@ const PastLaunchesType = new GraphQLObjectType({
   fields: () => ({
     flight_number: { type: GraphQLInt },
     mission_name: { type: GraphQLString },
-    launch_year: { type: GraphQLString }
-    /* rocket: { type: new GraphQLList(RocketType) } */
+    launch_year: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    details: { type: GraphQLString },
+    rocket: { type: RocketType }
+  })
+});
+
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
   })
 });
 
@@ -34,6 +61,15 @@ const RootQuery = new GraphQLObjectType({
             console.log(res.data);
             return res.data;
           });
+      }
+    },
+    rocket: {
+      type: new GraphQLList(RocketType),
+      resolve(parentValue, args) {
+        return axios.get('https://api.spacexdata.com/v3/rockets').then(res => {
+          console.log(res.data);
+          return res.data;
+        });
       }
     }
   }
